5da4b86769e8f9b0fd444dd4d0a6edf3c8f9f3f7
Imię i nazwiskoPiotr <raszta9999@wp.pl>
2018-07-30 18:06:39 +0200

    HTML SASS JS

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
+
+var c = require('ansi-colors');
+var notifier = require('node-notifier');
+
+function showError(err) {
+
+  notifier.notify({
+    title: 'Błąd kompilacji SCSS',
+    message: err.messageFormatted
+  });
+
+  console.log( c.red('==================') );
+  console.log( c.red(err.messageFormatted) );
+  console.log( c.red('==================') );
+  this.emit('end');
+
+}
+
+gulp.task('sass', function() {
+  gulp.src('scss/**/*.scss')
+    .pipe(plumber({
+      errorHandler : showError
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('scss/**/*.scss', ['sass'])
+});
+
+
+gulp.task('default', ['sass', 'watch'])
---
+++ js/app.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function (){
+
+  var prevBtn = document.querySelector("#prevPicture");
+  var nextBtn = document.querySelector("#nextPicture");
+
+  var listEl = document.querySelectorAll(".slider__item");
+  var articleEl = document.querySelectorAll(".slider__article");
+  var counter = 0;
+
+  listEl[counter].classList.add("visible");
+  articleEl[counter].classList.add("visible");
+
+  nextBtn.addEventListener("click", function(e){
+
+      listEl[counter].classList.remove("visible");
+      articleEl[counter].classList.remove("visible");
+          counter++;
+      if(counter>=listEl.length){
+          counter=0;
+      }
+
+      listEl[counter].classList.add("visible");
+      articleEl[counter].classList.add("visible");
+  });
+
+  prevBtn.addEventListener("click", function(e){
+
+      listEl[counter].classList.remove("visible");
+      articleEl[counter].classList.remove("visible");
+      counter--;
+      if(counter<0){
+      counter=listEl.length-1;
+      }
+      listEl[counter].classList.add("visible");
+      articleEl[counter].classList.add("visible");
+  });
+
+
+
+
+});
