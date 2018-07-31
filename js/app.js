1ecdc5d1633e6f5a1c3b22a9c4449a0ffd3d83bf
Imię i nazwiskoPiotr <raszta9999@wp.pl>
2018-07-31 18:26:57 +0200

    Adding RWD in progress

--- js/app.js
+++ js/app.js
@@ -35,6 +35,20 @@ document.addEventListener("DOMContentLoaded", function (){
       articleEl[counter].classList.add("visible");
   });
 
+  var btn = document.querySelector('#menu-trigger');
+  var menu = document.querySelector('.header__wrap');
+  var mobile = window.matchMedia("screen and (min-width: 1024px)")
+
+  btn.addEventListener('click', function(ev) {
+    ev.preventDefault();
+    menu.classList.toggle('open');
+  });
+
+  mobile.addListener( function(mobile) {
+    if (mobile.matches) {
+      menu.classList.remove('open');
+    }
+  });
 
 
 
