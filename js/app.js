dcdb4f883d83c96cd7ce6c1461ccb5112fba56a4
Imię i nazwiskoPiotr <raszta9999@wp.pl>
2018-09-10 19:43:28 +0200

    Adding interval to slider

--- js/app.js
+++ js/app.js
@@ -6,25 +6,38 @@ document.addEventListener("DOMContentLoaded", function (){
   var listEl = document.querySelectorAll(".slider__item");
   var articleEl = document.querySelectorAll(".slider__article");
   var counter = 0;
+  var time = 4000;
 
   listEl[counter].classList.add("visible");
   articleEl[counter].classList.add("visible");
 
+    var sliderId = setInterval(function () {
+        listEl[counter].classList.remove("visible");
+        articleEl[counter].classList.remove("visible");
+        counter++;
+        if (counter >= listEl.length) {
+            counter = 0;
+        }
+
+        listEl[counter].classList.add("visible");
+        articleEl[counter].classList.add("visible");
+    }, time)
+
   nextBtn.addEventListener("click", function(e){
+        time=4000;
+        listEl[counter].classList.remove("visible");
+        articleEl[counter].classList.remove("visible");
+        counter++;
+        if (counter >= listEl.length) {
+            counter = 0;
+        }
 
-      listEl[counter].classList.remove("visible");
-      articleEl[counter].classList.remove("visible");
-          counter++;
-      if(counter>=listEl.length){
-          counter=0;
-      }
-
-      listEl[counter].classList.add("visible");
-      articleEl[counter].classList.add("visible");
+        listEl[counter].classList.add("visible");
+        articleEl[counter].classList.add("visible");  
+        
   });
 
-  prevBtn.addEventListener("click", function(e){
-
+  prevBtn.addEventListener("click", function(e){       
       listEl[counter].classList.remove("visible");
       articleEl[counter].classList.remove("visible");
       counter--;
