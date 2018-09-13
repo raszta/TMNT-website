260a8ff58cd23d596031494b49b1b61345c6e3eb
Imię i nazwiskoPiotr <raszta9999@wp.pl>
2018-09-13 13:41:07 +0200

    Adding sticky menu, changes in js slider

--- js/app.js
+++ js/app.js
@@ -1,51 +1,61 @@
 document.addEventListener("DOMContentLoaded", function (){
 
-  var prevBtn = document.querySelector(".slider__button--prev");
-  var nextBtn = document.querySelector(".slider__button--next");
+    const prevBtn = document.querySelector(".slider__button--prev");
+    const nextBtn = document.querySelector(".slider__button--next");
 
-  var listEl = document.querySelectorAll(".slider__item");
-  var articleEl = document.querySelectorAll(".slider__article");
-  var counter = 0;
-  var time = 4000;
-
+    const listEl = document.querySelectorAll(".slider__item");
+    const articleEl = document.querySelectorAll(".slider__article");
+    let counter = 0;
+    const time = 4000;
+    let sliderId = setInterval(function () {
+        nextSlide();
+    }, time)
   listEl[counter].classList.add("visible");
   articleEl[counter].classList.add("visible");
 
-    var sliderId = setInterval(function () {
-        listEl[counter].classList.remove("visible");
-        articleEl[counter].classList.remove("visible");
-        counter++;
-        if (counter >= listEl.length) {
-            counter = 0;
-        }
-
-        listEl[counter].classList.add("visible");
-        articleEl[counter].classList.add("visible");
-    }, time)
-
   nextBtn.addEventListener("click", function(e){
-        time=4000;
-        listEl[counter].classList.remove("visible");
-        articleEl[counter].classList.remove("visible");
-        counter++;
-        if (counter >= listEl.length) {
-            counter = 0;
-        }
-
-        listEl[counter].classList.add("visible");
-        articleEl[counter].classList.add("visible");  
-        
+        clearInterval(sliderId);
+        nextSlide();
+      sliderId = setInterval(function () {
+          nextSlide();
+      }, time)
   });
 
-  prevBtn.addEventListener("click", function(e){       
+  prevBtn.addEventListener("click", function(e){ 
+      clearInterval(sliderId);
+      prevSlide();
+      sliderId = setInterval(function () {
+          nextSlide();
+      }, time)
+  });
+
+  function nextSlide(){
       listEl[counter].classList.remove("visible");
       articleEl[counter].classList.remove("visible");
-      counter--;
-      if(counter<0){
-      counter=listEl.length-1;
+      counter++;
+      if (counter >= listEl.length) {
+          counter = 0;
       }
       listEl[counter].classList.add("visible");
       articleEl[counter].classList.add("visible");
-  });
+  }
+  function prevSlide(){
+      listEl[counter].classList.remove("visible");
+      articleEl[counter].classList.remove("visible");
+      counter--;
+      if (counter < 0) {
+          counter = listEl.length-1;
+      }
+      listEl[counter].classList.add("visible");
+      articleEl[counter].classList.add("visible");
+  }
 
+    const header = document.querySelector('.header');
+    window.addEventListener('scroll', function (e) {
+        if (this.pageYOffset >= 40) {
+            header.classList.add('sticky');
+        } else {
+            header.classList.remove('sticky');
+        }
+    })
 });
